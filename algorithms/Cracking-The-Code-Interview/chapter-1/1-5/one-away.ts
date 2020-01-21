99047bc81c753b17deb558cb5087bd2a577dfc30
Paul Coroneos <paul@pcoroneos.com>
2020-01-21 13:02:49 -0600

    wip

--- algorithms/Cracking-The-Code-Interview/chapter-1/1-5/one-away.ts
+++ algorithms/Cracking-The-Code-Interview/chapter-1/1-5/one-away.ts
@@ -1,6 +1,13 @@
 const oneAway = (stra: string, strb: string) => {
+  const straArr = [...stra.toLowerCase()];
   if (stra.length > strb.length + 1 || strb.length > stra.length + 1) return false;
-  if (stra === strb) return [...stra].filter(char => !strb.includes(char)).length > 1;
+  if (stra === strb) return straArr.filter(char => !strb.includes(char)).length > 1;
+  let count = 0;
+  straArr.forEach(char => {
+    if (strb.includes(char)) count++;
+  })
+
+  return count > 1;
 }
 
 export default oneAway;
