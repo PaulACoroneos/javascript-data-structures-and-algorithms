9ac559a6519b321149abc729869d53b82cf9e40a
Paul Coroneos <paul@pcoroneos.com>
2020-04-11 22:48:05 -0500

    implement queue using array methods

--- data-structures/linked-list/linked-list.ts
+++ data-structures/linked-list/linked-list.ts
@@ -1,67 +1,66 @@
 interface Head {
-  value: any;
-  next: any;
+    value: any;
+    next: any;
 }
 
 export default class LinkedList {
+    private head: Head;
+    private tail: any;
 
-  private head: Head;
-  private tail: any;
-
-  constructor(value: any) {
-    this.head = { value: value, next: null };
-    this.tail = this.head;
-  }
-
-  append(value: any) {
-    const node = { value, next: null };
-    if (!this.head.next) this.head.next = node;
-    this.tail.next = node;
-    this.tail = node;
-    return this;
-  }
-
-  removeNode(value: any) {
-    let currentNode = this.head;
-    let previousNode = { value: null, next: null };
-    while (currentNode) {
-      if (currentNode.value === value) {
-        break;
-      }
-      previousNode = currentNode;
-      currentNode = currentNode.next;
+    constructor(value: any) {
+        this.head = { value: value, next: null };
+        this.tail = this.head;
     }
 
-    if (!currentNode) return false;
-
-    if (currentNode === this.head) {
-      this.head = this.head.next;
-      return;
-    }
-    previousNode.next = currentNode.next;
-  }
-
-  removeTail() {
-    let currentNode = this.head;
-    let previousNode = { value: null, next: null };
-
-    while (currentNode) {
-      if (!currentNode.next) break;
-      previousNode = currentNode;
-      currentNode = currentNode.next;
+    append(value: any) {
+        const node = { value, next: null };
+        if (!this.head.next) this.head.next = node;
+        this.tail.next = node;
+        this.tail = node;
+        return this;
     }
 
-    previousNode.next = null;
-    this.tail = previousNode;
-  }
+    removeNode(value: any) {
+        let currentNode = this.head;
+        let previousNode = { value: null, next: null };
+        while (currentNode) {
+            if (currentNode.value === value) {
+                break;
+            }
+            previousNode = currentNode;
+            currentNode = currentNode.next;
+        }
 
-  print() {
-    const printArr = [];
-    let currentNode = this.head;
-    while (currentNode) {
-      printArr.push(currentNode.value)
-      currentNode = currentNode.next;
+        if (!currentNode) return false;
+
+        if (currentNode === this.head) {
+            this.head = this.head.next;
+            return;
+        }
+        previousNode.next = currentNode.next;
     }
-    return printArr;
-  }
+
+    removeTail() {
+        let currentNode = this.head;
+        let previousNode = { value: null, next: null };
+
+        while (currentNode) {
+            if (!currentNode.next) break;
+            previousNode = currentNode;
+            currentNode = currentNode.next;
+        }
+
+        previousNode.next = null;
+        this.tail = previousNode;
+    }
+
+    print() {
+        const printArr = [];
+        let currentNode = this.head;
+        while (currentNode) {
+            printArr.push(currentNode.value);
+            currentNode = currentNode.next;
+        }
+        return printArr;
+    }
 }
