3170114ea23cdd98dd2e5a1425d453b24898bcf9
Paul Coroneos <paul@pcoroneos.com>
2020-04-27 14:29:44 -0500

    implement min stack using arrays

--- data-structures/stack/stack.ts
+++ data-structures/stack/stack.ts
@@ -1,27 +1,26 @@
 interface Storage {
-  [key: number]: any;
+    [key: number]: any;
 }
 
 export default class Stack {
+    private _storage: Storage;
+    private _length: number;
 
-  private _storage: Storage;
-  private _length: number;
+    constructor() {
+        this._storage = {};
+        this._length = 0;
+    }
 
-  constructor() {
-    this._storage = {};
-    this._length = 0;
-  }
+    push(value: any) {
+        this._storage[this._length++] = value;
+    }
 
-  push(value: any) {
-    this._storage[this._length++] = value;
-  }
+    pop() {
+        if (this._length < 1) return undefined;
+        return delete this._storage[--this._length];
+    }
 
-  pop() {
-    if (this._length < 1) return undefined;
-    return delete this._storage[--this._length];
-  }
-
-  peek() {
-    return this._storage[this._length - 1];
-  }
+    peek() {
+        return this._storage[this._length - 1];
+    }
 }
