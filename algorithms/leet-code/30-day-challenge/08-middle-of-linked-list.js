d6291df08d4392c810d493337796660328dc91d3
Paul Coroneos <paul@pcoroneos.com>
2020-04-26 21:57:13 -0500

    solved 30 day problem 8

---
+++ algorithms/leet-code/30-day-challenge/08-middle-of-linked-list.js
@@ -0,0 +1,20 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+export const middleNode = function (head) {
+    let slow = head;
+    let fast = head;
+    while (fast && fast.next !== null) {
+        fast = fast.next.next;
+        slow = slow.next;
+    }
+    return slow;
+};
